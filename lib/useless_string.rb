eae3ea427ba9b16b9ae9c6b189e219a46a9ec13e
Azam <azamikram101@yahoo.com>
2016-09-03 18:32:09 +0500

    Added more option to skip method.

--- lib/useless_string.rb
+++ lib/useless_string.rb
@@ -10,11 +10,41 @@ class String
 
   private
     def set_options(str, other_str, options)
+      remove_carriage_return(str, other_str) if options[:carriage_return]
+      remove_line_feed(str, other_str) if options[:line_feed]
+      remove_end_of_line(str, other_str) if options[:end_of_line]
       remove_spaces(str, other_str) if options[:spaces]
+      remove_numbers(str, other_str) if options[:numbers]
+      remove_special_characters(str, other_str) if options[:special_characters]
+    end
+
+    def remove_carriage_return(str, other_str)
+      str.gsub!(/\r/, '')
+      other_str.gsub!(/\r/, '')
+    end
+
+    def remove_line_feed(str, other_str)
+      str.gsub!(/\n/, '')
+      other_str.gsub!(/\n/, '')
+    end
+
+    def remove_end_of_line(str, other_str)
+      str.gsub!(/\r\n/, '')
+      other_str.gsub!(/\r\n/, '')
     end
 
     def remove_spaces(str, other_str)
       str.gsub!(/ /, '')
       other_str.gsub!(/ /, '')
+    end
+
+    def remove_numbers(str, other_str)
+      str.gsub!(/\d/, '')
+      other_str.gsub!(/\d/, '')
+    end
+
+    def remove_special_characters(str, other_str)
+      str.gsub!(/[^\da-z]+/i, '')
+      other_str.gsub!(/[^\da-z]+/i, '')
     end
 end
